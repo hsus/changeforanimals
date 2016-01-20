793d87a56f7f2b614d68b5306ac497623564d604
Joe Komenda <komejo@gmail.com>
2016-01-19 18:32:14 -0800

    Move intro text into data file for ease of editing, create read more toggle animations.

--- assets/js/hsus.js
+++ assets/js/hsus.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-  
+  $('.readmore').click(function() {
+    var el = $(this),
+        wrapper = el.parent().parent();
+    wrapper.toggleClass('showmore showless');
+    $('.showmore').find('.readmore').text('Read Less');
+    $('.showless').find('.readmore').text('Read More');
+  });
 
 
-}
+});
