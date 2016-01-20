0841f002132d971c11025f927144639b8a88f6d9
Joe Komenda <komejo@gmail.com>
2016-01-20 11:46:15 -0800

    Working read more animation effect

--- assets/js/hsus.js
+++ assets/js/hsus.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
   $('.readmore').click(function() {
     var el = $(this),
         wrapper = el.parent().parent();
+      $('.showmore .more').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        $(this).hide();
+      });
+      $('.showless .more').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        $(this).css('display','inline');
+      });
     wrapper.toggleClass('showmore showless');
     $('.showmore').find('.readmore').text('Read Less');
     $('.showless').find('.readmore').text('Read More');
+    $('.more').css('display','inline');
   });
 
 
