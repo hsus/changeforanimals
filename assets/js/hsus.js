8f7ea4194fdb515d6e6a5c4599fdf24c51b18cbd
Joe Komenda <komejo@gmail.com>
2016-01-27 11:44:14 -0800

    Fun with icons

--- assets/js/hsus.js
+++ assets/js/hsus.js
@@ -157,4 +157,26 @@ $(document).ready(function() {
       $('#mobile-menu').fadeOut();
   });
 
+  // Find images of class 'svg' and embed code directly.
+  $('img.svg').each(function(){
+    var $img = $(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+      var $svg = $(data).find('svg');
+      if(typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID);
+      }
+      if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+      $svg = $svg.removeAttr('xmlns:a');
+      $img.replaceWith($svg);
+    }, 'xml');
+  });
+
+
+
 });
