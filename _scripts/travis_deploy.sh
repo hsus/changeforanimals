b85fba3877e77cf57bfb5fbaec05eb5fe3f8da3b
Eric Paxton <heypaxton@gmail.com>
2016-01-29 14:09:09 -0800

    Update deploy script

--- _scripts/travis_deploy.sh
+++ _scripts/travis_deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+  echo TRAVIS_BRANCH
   bundle exec rake stage
 elif [[ $TRAVIS_BRANCH == 'live' ]] ; then
+  echo TRAVIS_BRANCH
   bundle exec rake publish
 else
   echo 'Invalid branch. You can only deploy from master and live.'
